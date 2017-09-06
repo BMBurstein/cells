2b4cf08d57642fc9b1560b328360ee1e5a89d592
baruch <bmburstein@gmail.com>
2017-09-06 10:33:14 +0300

    Infinite board

--- cell_sim1/main.cpp
+++ cell_sim1/main.cpp
@@ -1,46 +1,74 @@
 #include <SFML/Graphics.hpp>
 
-#include <vector>
+#include <unordered_map>
+#include <functional>
 
-static const unsigned int WIDTH = 800;
-static const unsigned int HEIGHT = 600;
+struct PointHash {
+  std::size_t operator()(sf::Vector2i const& v) const {
+    return std::hash<int>{}(v.x) ^ v.y;
+  }
+};
 
 class CellWorld : public sf::Drawable, public sf::Transformable
 {
-public:
-  static const std::size_t NUM_CELLS = HEIGHT * WIDTH;
+private:
 
+  struct Cell {
+    bool alive = false;
+    operator sf::Color() { return alive ? sf::Color::Black : sf::Color::White; }
+  };
+
+public:
   CellWorld() :
-    m_vertices(sf::Points, NUM_CELLS),
-    m_cells(NUM_CELLS),
-    m_new_cells(NUM_CELLS)
+    m_vertices(sf::Points)
   {
-    for (std::size_t i = 1; i < WIDTH - 1; ++i) {
-      for (std::size_t j = 1; j < HEIGHT - 1; ++j) {
-        m_vertices[i*HEIGHT + j] = { sf::Vector2f(i, j), sf::Color::White };
-        m_cells[i*HEIGHT + j].alive = false;// std::rand() % 2;
-      }
-    }
-    m_cells[400 * HEIGHT + 300].alive = m_cells[399 * HEIGHT + 301].alive = m_cells[400 * HEIGHT + 301].alive = m_cells[400 * HEIGHT + 302].alive = m_cells[401 * HEIGHT + 300].alive = true;
-    for (std::size_t i = 1; i < NUM_CELLS - 1; ++i) {
-      m_vertices[i].color = m_cells[i];
-    }
+    addCell(1, 0);
+    addCell(2, 0);
+    addCell(0, 1);
+    addCell(1, 1);
+    addCell(1, 2);
+    m_cells.swap(m_new_cells);
   }
 
   void update()
   {
-    auto cell = [&](int x, int y) { return m_cells[x * HEIGHT + y].alive ? 1 : 0; };
-
-    for (std::size_t i = 1; i < WIDTH-1; ++i) {
-      for (std::size_t j = 1; j < HEIGHT-1; ++j) {
-        auto adjacent = cell(i - 1, j - 1) + cell(i, j - 1) + cell(i + 1, j - 1)
-                      + cell(i - 1, j    ) +                + cell(i + 1, j    )
-                      + cell(i - 1, j + 1) + cell(i, j + 1) + cell(i + 1, j + 1);
-        m_new_cells[i*HEIGHT + j].alive = adjacent == 3 || (adjacent == 2 && cell(i,j));
-        m_vertices[i*HEIGHT + j].color = m_new_cells[i*HEIGHT + j];
+    m_new_cells.clear();
+    for (auto& c : m_cells) {
+      auto x = c.first.x;
+      auto y = c.first.y;
+      auto adjacent = cell(x - 1, y - 1).alive + cell(x, y - 1).alive + cell(x + 1, y - 1).alive
+                    + cell(x - 1, y    ).alive +           0          + cell(x + 1, y    ).alive
+                    + cell(x - 1, y + 1).alive + cell(x, y + 1).alive + cell(x + 1, y + 1).alive;
+      if (adjacent == 3 || (adjacent == 2 && c.second.alive)) {
+        addCell(x, y);
       }
     }
+
     m_cells.swap(m_new_cells);
+
+    m_vertices.clear();
+    for (auto& c : m_cells) {
+      if (c.second.alive) {
+        m_vertices.append({ { float(c.first.x),float(c.first.y) }, c.second });
+      }
+    }
+  }
+
+  Cell cell(int x, int y) {
+    auto it = m_cells.find({ x, y });
+    if (it == m_cells.end()) {
+      return Cell();
+    }
+    return it->second;
+  }
+
+  void addCell(int x, int y) {
+    m_new_cells[{x, y}].alive = true;
+    for (int i = x - 1; i <= x + 1; ++i) {
+      for (int j = y - 1; j <= y + 1; ++j) {
+        m_new_cells[{i, j}];//.alive &= true;
+      }
+    }
   }
 
 protected:
@@ -49,33 +77,31 @@ protected:
   {
     // apply the transform
     states.transform *= getTransform();
-
-    // our particles don't use a texture
+    
     states.texture = NULL;
 
     // draw the vertex array
+    target.clear(sf::Color::White);
     target.draw(m_vertices, states);
   }
 
 private:
-
-  struct Cell
-  {
-    bool alive;
-    operator sf::Color() { return alive ? sf::Color::Black : sf::Color::White; }
-  };
-
+  typedef std::unordered_map<sf::Vector2i, Cell, PointHash> CellsMap;
   sf::VertexArray m_vertices;
-  std::vector<Cell> m_cells;
-  std::vector<Cell> m_new_cells;
+  CellsMap m_cells;
+  CellsMap m_new_cells;
 };
+
+static const unsigned int WIDTH = 800;
+static const unsigned int HEIGHT = 600;
 
 int main()
 {
-  sf::RenderWindow window(sf::VideoMode(WIDTH, HEIGHT), "Cell Sim v0.1", sf::Style::Close);
+  sf::RenderWindow window(sf::VideoMode(WIDTH, HEIGHT), "Game of Life v0.2", sf::Style::Close);
 
   // create the particle system
   CellWorld cells;
+  cells.setPosition(WIDTH / 2, HEIGHT / 2);
 
   // create a clock to track the elapsed time
   sf::Clock clock;
